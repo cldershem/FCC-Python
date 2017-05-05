736c8eed69ed50ec259962e0f18fea698f86bb17
Cameron Dershem <cldershem@gmail.com>
2017-05-05 12:37:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This repo is for the Free Code Camp Python curriculum. Everything here is a
+reference to [this issue](https://github.com/freeCodeCamp/freeCodeCamp/issues/14588) on
+the FCC repo.
+
+There is no particular order to the lessons as of yet. Each lesson is a separate
+module and should be treated as independent of each other.
---
+++ intro.py
@@ -0,0 +1,44 @@
+"""
+introduciton
+~~~~~~~~~~~~
+
+Python is a general purpose programming language with a wide array of uses. It
+is known for its large standard library (batteries included) and huge number of
+third party libraries available.
+
+The sytntax for Python has very few keywords and symbols, making it read a lot
+like English or 'psuedo code'. One unique characterstic of Python is its use of
+whitespace instead of curly braces to seperate blocks.
+
+While searching for help and more information one might come across some older
+posts about a rift in the Python community regarding versions. As of the
+writing of this introduction, there are two officially supported versions of
+Python, 2.7 and 3.6. This tutorial will be teaching Python while occassionally
+pointing out differences that users may need to be aware of if they come across
+legacy versions of Python code. Most of the differences are minor and all of
+them are for the better. Any arguments you still see on the internet can be
+chalked up to old men yelling at clouds.
+
+While Python has been around since 1991, it has recently grown widely in
+popularity as it has become a favorite for teaching, rapid prototypes, science,
+websites, and data science.
+"""
+
+# Getting Started
+"""
+Python is an interpretted language, more like JavaScript than C, if you're on
+Mac or Linux you've already got a Python interpretter installed, if you're on
+Windows it might take an extra step or two. If you'd like to follow along on
+your own machine you can follows [this instructions]() to get Python 3.6
+installed.
+
+However, for the time being at least, we will be using an interpretter embeded
+in this webpage.
+"""
+
+# The `>>> ` symbol is the prompt in the Python interpretter
+>>> print('Hello, World!')
+Hello, World!
+
+
+# Give it a try!
